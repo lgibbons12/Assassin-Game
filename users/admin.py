f9b814af07cf90275fe34346e81a668c72343613
Liam Gibbons <liamwgibbons@gmail.com>
2024-01-14 21:56:00 -0500

    now done with saving data!

--- users/admin.py
+++ users/admin.py
@@ -1,25 +1,44 @@
 from django.contrib import admin
 from .models import CustomUser, Player, Checker
 from .game import GameManager
+from django.http import HttpResponse
+import csv
 
 class PlayerAdmin(admin.ModelAdmin):
-    actions = ['kill_player', 'assign_targets']
+    actions = ['assign_targets', 'save_info']
     list_display = ['user_name', 'is_dead', 'target_name', 'kills', 'is_playing', 'in_waiting']
 
     def assign_targets(self, request, queryset):
         gm = GameManager()
         gm.assign_targets()
 
-    def kill_player(self, request, queryset):
-        for player in queryset:
-            target = Player.objects.get(pk=player.target_pk)
-            print(f"{player.user.name} is going to kill their target {target.user.name}")
-            print(f"The target's target is {target.target_name}, so it should be the players after")
-            target.get_killed()
-            player.kill_target()
+    
 
     def user_name(self, obj):
         return obj.user.name
+    
+    def save_info(self, request, queryset):
+        # Define the response as a CSV file
+        response = HttpResponse(content_type='text/csv')
+        file_path = 'static/users/players_export.csv'
+        response['Content-Disposition'] = f'attachment; filename="{file_path}"'
+
+
+        # Create a CSV writer
+        csv_writer = csv.writer(response)
+
+        # Write header row
+        header_row = ['User ID', 'Username', 'Email', 'Is Dead', 'Target Name', 'Target PK', 'Kills', 'Is Playing', 'Is Winner', 'In Waiting']
+        csv_writer.writerow(header_row)
+
+        # Write data rows
+        for player in queryset:
+            data_row = [player.user.id, player.user.username, player.user.email, player.is_dead, player.target_name,
+                        player.target_pk, player.kills, player.is_playing, player.is_winner, player.in_waiting]
+            csv_writer.writerow(data_row)
+
+        return response
+
 
     user_name.short_description = 'User Name'
 
